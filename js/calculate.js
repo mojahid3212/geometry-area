a7c4fbc38caa38925f231101eb421c39d52da678
mojahid3212 <mojahidult17@gmail.com>
2023-11-23 00:54:05 +0600

    task done

--- js/calculate.js
+++ js/calculate.js
@@ -1,4 +1,7 @@
 
+document.getElementById('blog-btn').addEventListener('click',function(){
+    window.location.href="http://127.0.0.1:5500/blog.html"
+})
 // fuction to get the input area value/////
 
 function getElementInputValue(elementId){
@@ -7,23 +10,94 @@ function getElementInputValue(elementId){
     const elementInput=parseFloat(elementInputStr);
      return elementInput;
 }
-document.getElementById('tigngle-btn').addEventListener('click',function(){
+// calculate triangle area and fixed the value in area section///////
+document.getElementById('trigngle-btn').addEventListener('click',function(){
             const triangleBase=getElementInputValue('triangle-base');
             const triangleHeight=getElementInputValue('triangle-height');
             const triangleArea=0.5*triangleBase*triangleHeight;
             const  triangleAreaDecimal=triangleArea.toFixed(2);
-            addToCalculationEntry('triangle', triangleAreaDecimal);
-          
-            
+            if(isNaN(triangleAreaDecimal)){
+                alert('please input valid number');
+            }
+            else{
+                addToCalculationEntry('triangle', triangleAreaDecimal);
+            }   
+             
 })
 
+// calculate rectangle area and fixed the value in area section///////
+document.getElementById('rectangle-btn').addEventListener('click',function(){
+    const rectangleWidth= getElementInputValue('rectangle-width');
+    const rectangleLength= getElementInputValue('rectangle-length');
+    const rectangleArea=rectangleLength*rectangleWidth;
 
+    const rectangleAreaDecimal= rectangleArea.toFixed(2);
+    if(isNaN(rectangleAreaDecimal)){
+        alert("please !!! input the valid number")
+    }
+    else{
+        addToCalculationEntry('rectangle', rectangleAreaDecimal);
+    }
+})
+// ////calculat parallelogram area and fixed the value in area section//////
+document.getElementById('parallelogram-btn').addEventListener('click',function(){
+    const parallelogramBase=getElementInputValue('parallelogram-base');
+    const parallelogramHeight=getElementInputValue('parallelogram-height');
+    const parallelogramArea=parallelogramBase*parallelogramHeight;
+    const  parallelogramAreaDecimal=parallelogramArea.toFixed(2);
+    if(isNaN(parallelogramAreaDecimal)){
+        alert('please input valid number');
+    }
+    else{
+        addToCalculationEntry('parallelogram', parallelogramAreaDecimal);
+    }   
+     
+})
+// ////calculat rhombus area and fixed the value in area section//////
+document.getElementById('rhombus-btn').addEventListener('click',function(){
+    const d1=getElementInputValue('d1');
+    const d2=getElementInputValue('d2');
+    const rhombusArea=d1*d2;
+    const  rhombusAreaDecimal=rhombusArea.toFixed(2);
+    if(isNaN(rhombusAreaDecimal)){
+        alert('please input valid number');
+    }
+    else{
+        addToCalculationEntry('rhombus', rhombusAreaDecimal);
+    }   
+     
+})
 
+// ////calculat pentagon area and fixed the value in area section//////
+document.getElementById('pentagon-btn').addEventListener('click',function(){
+    const pentagonPeremeter=getElementInputValue('pentagon-perameter');
+    const pentagonBase=getElementInputValue('pentagon-base');
+    const pentagonArea=0.5*pentagonPeremeter*pentagonBase;
+    const  pentagonAreaDecimal=pentagonArea.toFixed(2);
+    if(isNaN(pentagonAreaDecimal)){
+        alert('please input valid number');
+    }
+    else{
+        addToCalculationEntry('pentagon', pentagonAreaDecimal);
+    }   
+     
+})
+// ////calculat ellipse area and fixed the value in area section//////
+document.getElementById('ellipse-btn').addEventListener('click',function(){
+    const ellipseExisA=getElementInputValue('ellipse-a-axis');
+    const ellipseExisB= getElementInputValue('ellipse-b-axis');
+    const pi=3.1416;
+    const ellipseArea=pi*ellipseExisA*ellipseExisB;
+    const ellipseAreaDecimal=ellipseArea.toFixed(2);
+    if(isNaN(ellipseAreaDecimal)){
+        alert("please !! input the valid number")
+    }
+    else{
+        addToCalculationEntry("ellipse",ellipseAreaDecimal);
+    }
+})
 
-
-
-
-// function to calculation result display /////
+//function to calculation result display /////
 function addToCalculationEntry(areaType, area){
     console.log(areaType+ ' '  + area)
     const calculationEntry = document.getElementById('calculation-entry');
@@ -32,7 +106,7 @@ function addToCalculationEntry(areaType, area){
 
     const p = document.createElement('p');
     p.classList.add('my-4');
-    p.innerHTML = `${count + 1}. ${areaType} ${area} cm<sup>2</sup> <span><button class="btn btn-sm btn-success">Convertm<sup>2</sup></button></span>`;
+    p.innerHTML = `${count + 1}. ${areaType}: ${area} cm<sup>2</sup> <span><button class=" bg-blue-500 px-1 rounded">Convert m<sup>2</sup></button></span>`;
 
     calculationEntry.appendChild(p);
 }
@@ -53,3 +127,4 @@ for(card of cards){
         this.style.backgroundColor=generateColor();
     })
 }
+
